a0724bd40d385d6c8ac72719f6991ee70c60c7da
Robson <robson.lsilva36@gmail.com>
2024-11-28 23:34:33 -0300

    Create project

---
+++ src/main/java/br/com/robson/api_spring_mongodb/ApiSpringMongodbApplication.java
@@ -0,0 +1,13 @@
+package br.com.robson.api_spring_mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpringMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/robson/api_spring_mongodb/ApiSpringMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.robson.api_spring_mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
