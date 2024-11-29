2aa021bd99304c8d8f498e7584497b6af7b66bfe
Robson <robson.lsilva36@gmail.com>
2024-11-29 00:42:47 -0300

    Entity User e RESTController funcionando

---
+++ src/main/java/br/com/robson/api_spring_mongodb/resources/UserResources.java
@@ -0,0 +1,25 @@
+package br.com.robson.api_spring_mongodb.resources;
+
+import br.com.robson.api_spring_mongodb.domain.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResources {
+
+    @RequestMapping(method=RequestMethod.GET)
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = new ArrayList<>();
+        User maria = new User("1001", "Maria Brown", "maria@gmail.com");
+        User alex = new User("1002", "Alex Green", "alex@gmail.com");
+        list.addAll(Arrays.asList(maria, alex));
+        return ResponseEntity.ok().body(list);
+    }
+}
